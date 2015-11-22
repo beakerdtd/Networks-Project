a27ab851402b32d4a1eba176edf6e8fdfe741577
beakerdtd <beakerdtd@gmail.com>
2015-11-21 22:27:02 -0500

    Cleaned up a bit. Added portfolio to financial intermdiaries and expanded deposit and withdraw

--- node.h
+++ node.h
@@ -14,63 +14,62 @@ class trader: public agent {
 
 		//create new portfolio
 		void createPortfolio() {
-			for (int j = 0; j < assets.size()-1; j++){       //assign values to the portfolio
-				portfolio[j].type = "node";
-				portfolio[j].ID = ID;
-				portfolio[j].title = assets[j];
-				portfolio[j].value = rand() % 100;
+			for (int i = 0; i < assets.size(); i++){         //assign values to the portfolio
+				portfolio[i].type = "node";
+				portfolio[i].ID = ID;
+				portfolio[i].title = assets[i];
+				portfolio[i].value = rand() % 100;             //randomize intial values of assets
 			}
-			portfolio[3].type = "node";
-			portfolio[3].ID = ID;
-			portfolio[3].title = assets[3];
-			portfolio[3].value = 0;
-		}
-
-		//return total value of all assets held by trader
-		double wealth() {                              
-			double val = 0;
-			for (int i = 0; i < assets.size(); i++){
-				if (getID == portfolio[i].ID){
-					val = val + portfolio[i].value;
-				}
-			}
-			return val;
+			portfolio[assets.size()-1].value = 0;            //set intial loans to zero
 		}
 
 		//function allowing an agent to deposit cash
 		void deposit_cash(agent* B, double amount){
-			double agent_cash_init = portfolio[0].value;                  //keep track of initial values
-			double fin_cash_init = B->accounts[getID].value;
+			double trader_cash_init = portfolio[0].value;                              //keep track of initial values
+			double account_cash_init = B->accounts[getID].asset[0].value;
+			double fin_cash_init = B->portfolio[0].value;
 
-			if (agent_cash_init < amount) {                               //if the agent cannot deposit desired amount
-				cout << "Insufficient funds" << '\n';                      	//restrict it from going into the negative
-				portfolio[0].value = agent_cash_init;
-				B->accounts[getID].value = fin_cash_init;
+			cout << "Agent attempted to deposited: " << amount << '\n';
+
+			if (trader_cash_init < amount) {                               						 //if the agent cannot deposit desired amount
+				cout << "Insufficient funds" << '\n';                      							 //restrict it from going into the negative
+				portfolio[0].value = trader_cash_init;
+				B->accounts[getID].asset[0].value = account_cash_init;
+				B->portfolio[0].value = fin_cash_init;
 			}
 			else {
-				portfolio[0].value = agent_cash_init - amount;             	//subtract amount deposited from agent wealth
-				B->accounts[getID].value = fin_cash_init + amount;          //add deposited amount to agent's account
+				portfolio[0].value = trader_cash_init - amount;             	           //subtract amount deposited from agent wealth
+				B->accounts[getID].asset[0].value = account_cash_init + amount;          //add deposited amount to agent's account
+				B->portfolio[0].value = fin_cash_init + amount;                          //and to financial intermediary's portfolio
+				
+				cout << "Agent deposited: " << amount << '\n';
 			}
 
 		}
 
 		//function allowing an agent to withdraw cash
 		void withdraw_cash(agent* B, double amount){
-			double agent_wealth_init = portfolio[0].value;                //keep track of initial values
-			double fin_wealth_init = B->accounts[getID].value;                    
+			double trader_cash_init = portfolio[0].value;                              //keep track of initial values
+			double account_cash_init = B->accounts[getID].asset[0].value;
+			double fin_cash_init = B->portfolio[0].value;
+ 
 
-			if (fin_wealth_init <= amount) {                              //if the agent wants to withdraw more than it has stored
-				cout << "Insufficient funds" << '\n';                       //restrict it from going into the negative
-				portfolio[0].value = agent_wealth_init;
-				B->accounts[getID].value = fin_wealth_init;
+			cout << "Agent attempted to withdraw: " << amount << '\n';                 
+
+			if (account_cash_init <= amount) {                              					 //if the agent wants to withdraw more than it has stored
+				cout << "Insufficient funds" << '\n';                      							 //restrict it from going into the negative
+				portfolio[0].value = trader_cash_init;
+				B->accounts[getID].asset[0].value = account_cash_init;
+				B->portfolio[0].value = fin_cash_init;
 			}
 			else{
-				portfolio[0].value = agent_wealth_init + amount;            //add amount withdrawn to agent wealth
-				B->accounts[getID].value = fin_wealth_init - amount;        //subtract withdrawn amount from agent's account
+				portfolio[0].value = trader_cash_init + amount;           						   //add amount withdrawn to agent wealth
+				B->accounts[getID].asset[0].value = account_cash_init - amount;          //subtract withdrawn amount from agent's account
+				B->portfolio[0].value = fin_cash_init - amount;                          //and from financial intermediary's portfolio
+
+				cout << "Agent withdrew: " << amount << '\n';
 			}
-
 		}
-
 };
 
 class financial_intermediary: public agent {
@@ -81,23 +80,35 @@ class financial_intermediary: public agent {
 		void createAccount(agent* A) {                                  
 			account b;
 			b.ID = A->getID;																							//assign ID
-			b.value = 0;                                                  //assign initial account sum
+		
+			for (int i = 0; i < assets.size(); i++){
+				b.asset.push_back(A->portfolio[i]);                         //add asset to portfolio
+				b.asset[i].type = A->portfolio[i].type;                     //and assign types
+				b.asset[i].ID = A->portfolio[i].ID;
+				b.asset[i].title = A->portfolio[i].title;
+				b.asset[i].value = 0;
+			}          
+			                                     
 			accounts.push_back(b);                                        //add account to list of accounts
 		}
 
-		//return total value of all assets held by agent
-		double wealth() {                                             
-			if (!accounts.empty()){
-				double val = 0;                                               
-				for (int i = 0; i < assets.size(); i++){
-					if (getID == accounts[i].ID){
-						val = val + accounts[i].value;
+		void createPortfolio() {
+			double val = 0;
+
+			for (int i = 0; i < assets.size(); i++){                      //assign values to the portfolio
+
+				portfolio[i].type = "financial intermediary";
+				portfolio[i].ID = ID;
+				portfolio[i].title = assets[i];
+				if (!accounts.empty()){
+					for (int j = 0; j < accounts.size(); j++) {
+					val = val + accounts[j].asset[i].value;
 					}
+				portfolio[i].value = val;
 				}
-				return val;
-			}
-			else {
-				return 0;
+				else {
+					portfolio[i].value = 0;
+				}
 			}
 		}
 }; 
